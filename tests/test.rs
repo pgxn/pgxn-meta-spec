c0b77217db77b457888e4110d761d5356a4a2aee
David E. Wheeler <david@justatheory.com>
2024-06-28 19:03:58 -0400

    Add valid and invalid test corpuses

--- tests/test.rs
+++ tests/test.rs
@@ -1,10 +1,9 @@
-use std::{
-    collections::HashMap,
-    error::Error,
-    fs::{self, File},
-};
+use std::fs::{self, File};
+use std::io::{prelude::*, BufReader};
+use std::{collections::HashMap, error::Error};
 
 use boon::{Compiler, Schemas};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[test]
@@ -34,11 +33,13 @@ fn test_schema_v1() -> Result<(), Box<dyn Error>> {
             } else {
                 panic!("Unable to find ID in {}", path.display());
             }
+        } else {
+            println!("Skipping {}", path.display());
         }
     }
 
     // Make sure we found schemas.
-    assert!(!loaded.is_empty());
+    assert!(!loaded.is_empty(), "No schemas loaded!");
 
     // Make sure each schema we loaded is valid.
     let mut schemas = Schemas::new();
@@ -51,7 +52,85 @@ fn test_schema_v1() -> Result<(), Box<dyn Error>> {
             if let Err(e) = schemas.validate(example, index) {
                 panic!("Example {i} failed: {e}");
             }
-            println!("  Example {i} ok");
+            // println!("  Example {i} ok");
+        }
+    }
+
+    Ok(())
+}
+
+fn new_compiler(dir: &str) -> Result<Compiler, Box<dyn Error>> {
+    let mut compiler = Compiler::new();
+    let paths = fs::read_dir(dir)?;
+    for path in paths {
+        let path = path?.path();
+        let bn = path.file_name().unwrap().to_str().unwrap();
+        if bn.ends_with(".schema.json") {
+            let schema: Value = serde_json::from_reader(File::open(path.clone())?)?;
+            if let Value::String(s) = &schema["$id"] {
+                // Add the schema to the compiler.
+                compiler.add_resource(s, schema.to_owned())?;
+            } else {
+                panic!("Unable to find ID in {}", path.display());
+            }
+        } else {
+            println!("Skipping {}", path.display());
+        }
+    }
+
+    Ok(compiler)
+}
+
+#[derive(Deserialize, Serialize)]
+struct CorpusCase {
+    test: String,
+    error: Option<String>,
+    meta: Value,
+}
+
+#[test]
+fn test_corpus_v1_valid() -> Result<(), Box<dyn Error>> {
+    // Load the schema and compile the root schema.
+    let mut compiler = new_compiler("schema/v1")?;
+    const SCHEMA_ID: &str = "https://pgxn.org/meta/v1/distribution.schema.json";
+    let mut schemas = Schemas::new();
+    let index = compiler.compile(SCHEMA_ID, &mut schemas)?;
+
+    // Test each meta JSON in the corpus.
+    let file = File::open("tests/corpus/v1/valid.txt")?;
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let tc: CorpusCase = serde_json::from_str(&line?)?;
+
+        if let Err(e) = schemas.validate(&tc.meta, index) {
+            panic!("{} failed: {e}", &tc.test);
+        }
+        println!("Example {} ok", &tc.test);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_corpus_v1_invalid() -> Result<(), Box<dyn Error>> {
+    // Load the schema and compile the root schema.
+    let mut compiler = new_compiler("schema/v1")?;
+    const SCHEMA_ID: &str = "https://pgxn.org/meta/v1/distribution.schema.json";
+    let mut schemas = Schemas::new();
+    let index = compiler.compile(SCHEMA_ID, &mut schemas)?;
+
+    // Test each meta JSON in the corpus.
+    let file = File::open("tests/corpus/v1/invalid.txt")?;
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let tc: CorpusCase = serde_json::from_str(&line?)?;
+        match schemas.validate(&tc.meta, index) {
+            Ok(_) => panic!("{} unexpectedly passed!", &tc.test),
+            Err(e) => assert!(
+                e.to_string().contains(&tc.error.unwrap()),
+                "{} error: {e}",
+                &tc.test,
+            ),
         }
     }
 
